6e4fdd0290641b8c7784d75d3fc9de8ac03521da
nonils <nonils@tutanota.com>
2023-01-26 02:33:02 -0300

    Innitial commit for app

---
+++ App.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { NativeBaseProvider, extendTheme, theme as nbTheme } from "native-base";
+import Config from "./nativebase.config";
+import { Platform } from "react-native";
+import {
+  Button,
+  IconButton,
+  Divider,
+  Input,
+  Radio,
+  Pressable,
+  TextArea,
+} from "./themes";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import OtpVerification from "./screens/OtpVerification";
+import ProductDetails from "./screens/ProductDetails";
+import Splash from "./screens/Splash";
+
+export default ({ children }: { children: React.ReactNode }) => {
+  const customTheme = extendTheme({
+    config: {
+      initialColorMode: "light",
+    },
+    colors: {
+      primary: nbTheme.colors.emerald,
+      customGray: "#2F3948",
+      secondary: nbTheme.colors.coolGray,
+    },
+    sizes: {
+      container: "1016px",
+    },
+    components: {
+      Button,
+      Radio,
+      Divider,
+      Input,
+      TextArea,
+    },
+  });
+
+  // Move this to index.html later
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      document.body.style.overflow = "hidden";
+      document.body.style.height = "100%";
+    }
+  }, []);
+
+  const Drawer = createDrawerNavigator();
+
+  return (
+    <NativeBaseProvider theme={customTheme} config={Config}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Splash"
+        >
+          <Drawer.Screen name={"Splash"} component={Splash} />
+          <Drawer.Screen name={"SignUp"} component={SignUp} />
+          <Drawer.Screen name={"SignIn"} component={SignIn} />
+          <Drawer.Screen name={"OtpVerification"} component={OtpVerification} />
+          <Drawer.Screen name={"ProductDetails"} component={ProductDetails} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+};
---
+++ screens/Splash.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Box, VStack, Button, Image, Center, Hidden } from "native-base";
+import GuestLayout from "../layouts/GuestLayout";
+
+type ActionButtonsType = {
+    signInAction: () => void;
+}
+
+function ActionButtons({signInAction}: ActionButtonsType) {
+  return (
+    <VStack space={4} mt={{ base: 10, md: 12 }}>
+      <Button variant="subtle" py={4} onPressOut={signInAction}>
+        Iniciar Sesion
+      </Button>
+      <Button
+        variant="outline"
+        py={4}
+        borderColor="secondary.100"
+        _text={{
+          color: "coolGray.50",
+        }}
+        _hover={{
+          bg: "primary.600",
+        }}
+        _pressed={{
+          bg: "primary.700",
+        }}
+      >
+        Registrarse
+      </Button>
+    </VStack>
+  );
+}
+function HeaderLogo() {
+  return (
+    <Box alignItems="center" justifyContent="center">
+      <Hidden from="md">
+        <Image
+          source={require("../assets/cajademedicos_logo.png")}
+          height={100}
+            width={270}
+          alt="Logo"
+        />
+      </Hidden>
+    </Box>
+  );
+}
+export default function Splash({navigation}:any) {
+    const moveToSignIn = () => {
+        navigation.navigate('SignIn');
+    }
+  return (
+    <GuestLayout>
+      <Center w="100%" flex={1}>
+        <Box
+          maxW="500"
+          w="100%"
+          height={{ md: "544" }}
+          px={{ base: 4, md: 8 }}
+          bg={{ md: "primary.600" }}
+          justifyContent="center"
+        >
+          <HeaderLogo />
+          <ActionButtons signInAction={moveToSignIn}/>
+        </Box>
+      </Center>
+    </GuestLayout>
+  );
+}
